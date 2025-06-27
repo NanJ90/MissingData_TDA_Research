3dc1cb4aad6471dec8da53bb19837daf67fa579c
NJ90 <jianan.beijing@gmail.com>
2025-06-27 09:57:56 -0400

    fix: update model building and add utils function

---
+++ utils.py
@@ -0,0 +1,225 @@
+# coding=utf-8
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''Utility functions for GAIN.
+
+(1) normalization: MinMax Normalizer
+(2) renormalization: Recover the data from normalzied data
+(3) rounding: Handlecategorical variables after imputation
+(4) rmse_loss: Evaluate imputed data in terms of RMSE
+(5) xavier_init: Xavier initialization
+(6) binary_sampler: sample binary random variables
+(7) uniform_sampler: sample uniform random variables
+(8) sample_batch_index: sample random batch index
+'''
+ 
+# Necessary packages
+import torch
+import numpy as np
+torch.manual_seed(42)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(42)
+
+def normalization (data, parameters=None):
+  '''Normalize data in [0, 1] range.
+  
+  Args:
+    - data: original data
+  
+  Returns:
+    - norm_data: normalized data
+    - norm_parameters: min_val, max_val for each feature for renormalization
+  '''
+  if isinstance(data, np.ndarray):
+    data = torch.from_numpy(data)
+  # Parameters
+  _, dim = data.shape
+  norm_data = data.clone()
+  
+  if parameters is None:
+  
+    # MixMax normalization
+    min_val = torch.zeros(dim)
+    max_val = torch.zeros(dim)
+    
+    # For each dimension
+    for i in range(dim):
+      mask = ~torch.isnan(norm_data[:,i])
+      min_val[i] = torch.min(norm_data[mask,i])
+      norm_data[:,i] = norm_data[:,i] - min_val[i]
+      max_val[i] = torch.max(norm_data[mask,i])
+      norm_data[:,i] = norm_data[:,i] / (max_val[i] + 1e-6)   
+      
+    # Return norm_parameters for renormalization
+    norm_parameters = {'min_val': min_val,
+                       'max_val': max_val}
+
+  else:
+    min_val = parameters['min_val']
+    max_val = parameters['max_val']
+    
+    # For each dimension
+    for i in range(dim):
+      norm_data[:,i] = norm_data[:,i] - min_val[i]
+      norm_data[:,i] = norm_data[:,i] / (max_val[i] + 1e-6)  
+      
+    norm_parameters = parameters    
+      
+  return norm_data, norm_parameters
+
+
+def renormalization (norm_data, norm_parameters):
+  '''Renormalize data from [0, 1] range to the original range.
+  
+  Args:
+    - norm_data: normalized data
+    - norm_parameters: min_val, max_val for each feature for renormalization
+  
+  Returns:
+    - renorm_data: renormalized original data
+  '''
+  if isinstance(norm_data, np.ndarray):
+    norm_data = torch.from_numpy(norm_data)
+
+  min_val = norm_parameters['min_val']
+  max_val = norm_parameters['max_val']
+
+  _, dim = norm_data.shape
+  renorm_data = norm_data.clone()
+    
+  for i in range(dim):
+    renorm_data[:,i] = renorm_data[:,i] * (max_val[i] + 1e-6)   
+    renorm_data[:,i] = renorm_data[:,i] + min_val[i]
+    
+  return renorm_data
+
+
+def rounding (imputed_data, data_x):
+  '''Round imputed data for categorical variables.
+  
+  Args:
+    - imputed_data: imputed data
+    - data_x: original data with missing values
+    
+  Returns:
+    - rounded_data: rounded imputed data
+  '''
+  if isinstance(imputed_data, torch.Tensor):
+    imputed_data = imputed_data.numpy()
+  if isinstance(data_x, torch.Tensor):
+    data_x = data_x.numpy()
+
+  _, dim = data_x.shape
+  rounded_data = imputed_data.copy()
+  
+  for i in range(dim):
+    temp = data_x[~np.isnan(data_x[:, i]), i]
+    # Only for the categorical variable
+    if len(np.unique(temp)) < 20:
+      rounded_data[:, i] = np.round(rounded_data[:, i])
+      
+  return rounded_data
+
+
+def rmse_loss (ori_data, imputed_data, data_m):
+  '''Compute RMSE loss between ori_data and imputed_data
+  
+  Args:
+    - ori_data: original data without missing values
+    - imputed_data: imputed data
+    - data_m: indicator matrix for missingness
+    
+  Returns:
+    - rmse: Root Mean Squared Error
+  '''
+  if isinstance(ori_data, np.ndarray):
+    ori_data = torch.from_numpy(ori_data)
+  if isinstance(imputed_data, np.ndarray):
+    imputed_data = torch.from_numpy(imputed_data)
+  if isinstance(data_m, np.ndarray):
+    data_m = torch.from_numpy(data_m)
+  
+  ori_data, norm_parameters = normalization(ori_data)
+  imputed_data, _ = normalization(imputed_data, norm_parameters)
+    
+  # Only for missing values
+  nominator = torch.sum(((1-data_m) * ori_data - (1-data_m) * imputed_data)**2)
+  denominator = torch.sum(1-data_m)
+  
+  rmse = torch.sqrt(nominator/float(denominator))
+  
+  return rmse
+
+
+def xavier_init(size):
+  '''Xavier initialization.
+  
+  Args:
+    - size: vector size
+    
+  Returns:
+    - initialized random vector.
+  '''
+  in_dim = size[0]
+  xavier_stddev = 1. / tf.sqrt(in_dim / 2.)
+  return tf.random_normal(shape = size, stddev = xavier_stddev)
+      
+
+def binary_sampler(p, rows, cols):
+  '''Sample binary random variables.
+  
+  Args:
+    - p: probability of 1
+    - rows: the number of rows
+    - cols: the number of columns
+    
+  Returns:
+    - binary_random_matrix: generated binary random matrix.
+  '''
+  unif_random_matrix = np.random.uniform(0., 1., size = [rows, cols])
+  binary_random_matrix = 1*(unif_random_matrix < p)
+  return binary_random_matrix
+
+
+def uniform_sampler(low, high, rows, cols):
+  '''Sample uniform random variables.
+  
+  Args:
+    - low: low limit
+    - high: high limit
+    - rows: the number of rows
+    - cols: the number of columns
+    
+  Returns:
+    - uniform_random_matrix: generated uniform random matrix.
+  '''
+  return torch.rand(rows, cols) * (high - low) + low
+
+
+def sample_batch_index(total, batch_size):
+  '''Sample index of the mini-batch.
+  
+  Args:
+    - total: total number of samples
+    - batch_size: batch size
+    
+  Returns:
+    - batch_idx: batch index
+  '''
+  total_idx = torch.randperm(total)
+  batch_idx = total_idx[:batch_size]
+  return batch_idx
+  
+
+  
